0dfeacaa98b8756de88f9fc4350e46acf0eedf63
fiery98 <yuanzilin@gmail.com>
2018-07-12 18:17:38 -0400

    add

--- exercise 1-03.c
+++ exercise 1-03.c
@@ -2,8 +2,9 @@
 
 int main() {
     printf("Fahrenheit  Celsius\n");
-    int f=0;
-    float c=0;
+    int f;
+    float c;
+    f=0;
     while(f<=300){
         c=5.0*(f-32)/9.0;
         printf("  %3d        %6.2f\n",f,c);
--- exercise 1-04.c
+++ exercise 1-04.c
@@ -2,8 +2,9 @@
 
 int main() {
     printf("Celsius      Fahrenheit\n");
-    int c=0;
+    int c;
     float f;
+    c=0;
     while(c<=300){
         f=9.0*c/5.0 +32;
         printf("  %3d           %6.2f\n",c,f);
