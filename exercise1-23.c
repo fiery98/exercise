cf3182e391d65ad4f550146a3a4c7ccbb3add721
fiery98 <yuanzilin@gmail.com>
2018-07-27 13:05:09 -0400

    add

--- exercise1-23.c
+++ exercise1-23.c
@@ -3,44 +3,42 @@
 int input(char output[]);
 
 int main() {
-    char line[200];
+    int i;
+    char line[2000]={0};
     while((input(line))>0){
         printf("%s",line);
+        for(i=0;i<2000&&line[i]!=0;i++)
+            line[i]=0;
     }
     return 0;
 }
 
-int input(char line[]){
-    int a,b,i,change1,change2,quit;
-    change1=0;
-    change2=0;
-    quit=0;
-    for(i=0;(a=getchar())!=EOF&&a!='\n'&&i<1999;i++){
-        if(a=='/'&&change1!=1){
-            change1=1;
-        }
-        else if(a=='/'&&change1==1){
-            line[i-1]='/';
-        }
-        else if(a=='*'&&change1==1){
-            while((b=getchar())!=EOF&&quit!=1){
-                if(b=='*'){
-                    change2=1;
-                }
-                else if(b=='/'&&change2==1){
-                    quit=1;
-                    change2=0;
-                }
-            }
-            i--;
-            if(b!='\n')
-                line[i]=b;
-            change1=0;
-            quit=0;
-        }
-        else{
+
+int input(char line[]){                                                                                                  /* input the character and return how many characters have in this line */
+    int a,b,i,StartComments,EndComments;
+    StartComments=0;
+    EndComments=0;
+    for(i=0;(a=getchar())!=EOF&&a!='\n'&&i<1999;i++) {
+        if(a=='/') {
             line[i]=a;
-            change1=0;
+            StartComments=1;
+        }
+        else if(a=='*'&&StartComments==1) {
+            while ((b=getchar())!=EOF) {
+                if (b=='*')
+                    EndComments = 1;
+                else if (b=='/'&&EndComments==1)
+                    break;
+                else
+                    EndComments=0;
+            }
+            StartComments=0;
+            EndComments=0;
+            i=i-2;
+        }
+        else {
+            line[i]=a;
+            StartComments=0;
         }
     }
     if(a=='\n'){
